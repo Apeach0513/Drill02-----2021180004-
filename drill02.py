6220320d2272f77ad9281bef377983252acb9622
Apeach0513 <ksiny020513@naver.com>
2023-09-20 17:14:23 +0900

    Drill02 제출(2021180004 김신영)

---
+++ drill02.py
@@ -0,0 +1,63 @@
+import os
+os.chdir('C:\\githud\\Drill2\\Lecture04_2D_Rendering')
+from pico2d import*
+
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+import math
+
+x=0
+y=90
+r=270
+while(1) :
+    while(x<800):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        x = x +2
+        delay(0.01)
+
+    while(y<600):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        y = y +2
+        delay(0.01)
+
+    while(x>0):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x - 2
+        delay(0.01)
+
+    while(y>90):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        y = y- 2
+        delay(0.01)
+
+    while(x<400):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x +2
+        delay(0.01)
+    
+    while(r>-90):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        r = r - 2
+        x = 255*math.cos(r/360*2*math.pi)+400
+        y = 255*math.sin(r/360*2*math.pi)+345
+        delay(0.05)
+        
+    
+
+close_canvas()
